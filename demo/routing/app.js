4aa56012978708e0e70f8bab73b1272b442899d1
Radik Fattakhov <radikft@gmail.com>
2014-05-17 23:56:04 +0400

    Update app.js

--- demo/routing/app.js
+++ demo/routing/app.js
@@ -1,8 +1,29 @@
 Ext.application({
     name: 'MyApp',
     appFolder: 'app',
-
+    
+    requires: ['Ext.panel.Panel', 'Ext.form.Label'],
     controllers: ['Navigation'],
 
-    defaultToken: 'home'
+    defaultToken: 'home',
+    
+    launch: function(){
+        Ext.create('Ext.container.Viewport', {
+            renderTo: Ext.getBody(),
+            layout: 'center',
+            style: {
+                backgroundColor: 'gray'
+            },
+            items: [{
+                xtype: 'panel',
+                items: [{
+                    xtype: 'label',
+                    content: 'hash',
+                    style: {
+                        fontSize: 40
+                    }
+                }]
+            }]
+        });
+    }
 });
